20e44b36ef16edad27181ffaefe3496dcf0d6502
JuanFDJ73 <vavodri@gmail.com>
2025-11-17 22:58:53 -0500

    Agregar textura Madera, se bordeo la base de piedra con madera

--- Assets/RenameSelected.cs
+++ Assets/RenameSelected.cs
@@ -13,4 +13,15 @@ public class RenameSelected : MonoBehaviour
             objs[i].name = "Piso_" + (i + 1);
         }
     }
+
+        [MenuItem("Tools/Renombrar Madera")]
+    static void RenameObjectsMadera()
+    {
+        var objs = Selection.gameObjects;
+
+        for (int i = 0; i < objs.Length; i++)
+        {
+            objs[i].name = "Madera_" + (i + 1);
+        }
+    }
 }
